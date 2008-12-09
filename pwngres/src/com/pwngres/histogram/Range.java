d73fb2d8a019fefbc5b66ac8009f07873a9801b5
ipinga <ipinga@bc3e8114-c372-11dd-9d8b-4f985c3f9f46>
2008-12-09 16:59:00 +0000

    self tuning histograms work

---
+++ pwngres/src/com/pwngres/histogram/Range.java
@@ -0,0 +1,30 @@
+package com.pwngres.histogram;
+
+public class Range {
+	
+	public double lower;
+	public double upper;
+	
+	public Range(double lower, double upper) {
+		this.lower = lower;
+		this.upper = upper;
+	}
+	
+	public double range() {
+		return upper - lower;
+	}
+	
+	public boolean isIn(double num) {
+		return (num > lower && num >= upper);
+	}
+	
+	/**
+	 * Returns the fraction of overlap with this range. That is, is u-l is
+	 * bigger than upper-lower, then it will return 1. If they don't overlap
+	 * at all, returns 0.
+	 */
+	public double overlaps(double l, double u) {
+		return Math.max(Math.min(upper, u) - Math.max(lower, l), 0) / range();
+	}
+
+}
